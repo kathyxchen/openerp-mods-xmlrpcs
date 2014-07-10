220dac4b98650ba1f39fb323ea2792e867718d3f
kchen17 <kchen2013@gmail.com>
2014-07-10 12:43:44 -0400

    Industry specific module

---
+++ example-modules/product_pcba/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Products PCBA Fields',
+    'version' : '1.0',
+    'author' : 'AAG',
+    'category' : 'Specific Industry Applications',
+    'depends' : ['stock', 'product', 'base'],
+    'demo' : [],
+    'description': """Add fields to product for PCBA components""",
+    'data' : ['product_pcba_view.xml'],
+    'auto_install': False,
+    'installable': True,
+    'images': [],
+}
